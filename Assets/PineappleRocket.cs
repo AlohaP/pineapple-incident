2afafd1bf80990061bb9c8197c54d8aeda1f4cf1
Piotr Górski <piotrek.gorski90@gmail.com>
2018-06-06 15:26:54 +0200

    Add script for desired behaviour of playing sound file

--- Assets/PineappleRocket.cs
+++ Assets/PineappleRocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PineappleRocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
@@ -22,13 +24,21 @@ public class PineappleRocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        } else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            //we acces transform component in Unity, wich is available to every game object
+            transform.Rotate(Vector3.forward);      // Vector3.forward makes object rotates on z axis        
         } else if (Input.GetKey(KeyCode.D)){
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
